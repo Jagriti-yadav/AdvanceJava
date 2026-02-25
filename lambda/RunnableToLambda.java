7e8ef7a32601c3acd1c9f06d0bbe25569fb9b65a
Jagriti-yadav <jagritiyadav1901@gmail.com>
2026-02-26 01:13:07 +0530

    lambda expression example

---
+++ lambda/RunnableToLambda.java
@@ -0,0 +1,27 @@
+package lambda;
+
+//Runnable is a functional Interface
+//Runnable r = new Runnable(){
+//public void run(){ System.out.println("running thread");}
+//};
+
+public class RunnableToLambda {
+
+  // public static void main(String[] args){
+
+  //   Runnable r = () -> System.out.println("running thread");
+  // Thread t = new Thread(r);
+  // t.start();
+  // }  
+
+  //Comparator<Integer> comp = (Integer a, Integer b)->a-b;
+
+  //Callable<Integer> c = ()->100;
+
+  //ActionListener al = (ActionEvent e)-> System.out.println("BUTTON CLICKED");
+
+  
+
+}
+
+
