7570c77895f548c0474c4d7fa048f47fbc160ac3
Jagriti-yadav <jagritiyadav1901@gmail.com>
2026-03-13 22:03:46 +0530

    var example

---
+++ varp/VarPractice.java
@@ -0,0 +1,15 @@
+package varp;
+//generic = specific datatype
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class VarPractice {
+  public static void main(String[] args){
+    // var list = new ArrayList<>(); = any data will get stored hence it must specify what kind of data is to stored
+    
+    var list = new ArrayList<Integer>();
+    var map = new HashMap<Integer,String>();
+    var number = 20;
+  }
+}
