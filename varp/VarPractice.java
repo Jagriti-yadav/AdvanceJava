fbea8b52df8b84c9bfa2f280a2c400b8d3a6a0b3
Jagriti-yadav <jagritiyadav1901@gmail.com>
2026-04-06 21:52:19 +0530

    var examples

--- varp/VarPractice.java
+++ varp/VarPractice.java
@@ -9,7 +9,14 @@ public class VarPractice {
     // var list = new ArrayList<>(); = any data will get stored hence it must specify what kind of data is to stored
     
     var list = new ArrayList<Integer>();
-    var map = new HashMap<Integer,String>();
+    var map = new HashMap<Integer, String>();
     var number = 20;
+
+        list.add(10);
+        map.put(1, "Hello");
+
+        System.out.println(list);
+        System.out.println(map);
+        System.out.println(number);
   }
 }
