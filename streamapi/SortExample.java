aa168ec3b395ca34b45f02a1a85d168e472ac6cd
Jagriti-yadav <jagritiyadav1901@gmail.com>
2026-05-18 23:50:04 +0530

    sorting

--- streamapi/SortExample.java
+++ streamapi/SortExample.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class SortExample {
   public static void main(String[] args){
     List<Integer> list = Arrays.asList(1,2,6,3,4);
-    List<Integer> sortedlist = list.stream().sorted().toList();
+    List<Integer> sortedlist = list.stream().sorted(Comparator.reverseOrder()).toList();
     System.out.println(sortedlist);
   }
 }
